95562593c8e19640df07fbfc04229aeef4ff0f3b
Gusan <gusan@Gusanui-MacBook-Pro.local>
2019-12-29 12:33:21 +0900

    add input code

--- Main.java
+++ Main.java
@@ -11,11 +11,36 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        List<Food> foodList = new ArrayList<>();
         int n = scan.nextInt();
         for(int i=0; i<n; i++) {
             for(int j=0; j<4; j++) {
-
+                foodList.add(new Food(scan.next(), scan.next(), scan.nextInt(), scan.nextInt()));
             }
         }
+        String todayFood = scan.nextLine();
+        System.out.println(mayEat() ? "O" : "X");
+    }
+
+    private static boolean mayEat() {
+        return false;
+    }
+}
+
+class Food {
+    private String foodName;
+    private String foodPrice;
+    private int peopleCnt;
+    private int stressLv;
+
+    public Food(String foodName, String foodPrice, int peopleCnt, int stressLv) {
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+        this.peopleCnt = peopleCnt;
+        this.stressLv = stressLv;
+    }
+
+    public Food getFood() {
+        return new Food(foodName, foodPrice, peopleCnt, stressLv);
     }
 }
--- Main.py
+++
@@ -1,7 +0,0 @@
-eatenCount = input("먹었던 음식 수 : ")
-i=0
-arr = []
-while i < eatenCount :
-    eaten = input("음식명, 음식의 가격, 인원 수, 먹은 후 스트레스 지수를 입력해주세요 : ")
-    arr.append(eaten)
-
