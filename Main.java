86cdafeda43c4ef8eb43b7357e00faf141fa681a
Gusan <gusan@Gusanui-MacBook-Pro.local>
2019-12-29 12:19:00 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<4; j++) {
+
+            }
+        }
+    }
+}
